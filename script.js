39aa02bd3865972ab97ace55c48e0c00b7fd864d
Stanislav Tsyhanok <stif.1974@gmail.com>
2024-05-20 15:41:49 +0300

    Add json file and all working

--- script.js
+++ script.js
@@ -1,111 +1,7 @@
 // Предполагается, что данные о событиях будут загружаться из API или базы данных.
-const events = [
-    {
-        id: 1,
-        title: "Событие 1",
-        description: "Описание события 1",
-        date: "2023-05-10",
-        organizer: "Организатор 1",
-        user: [{
-            name: "Alex",
-            email: 'alex@ghgjg.com'
-        },{
-            name: "Stif",
-            email: 'stif@ghgjg.com'
-        },{
-            name: "Dron",
-            email: 'dron@ghgjg.com'
-        },
 
-        ],
-    },
-    {
-        id: 2,
-        title: "Событие 2",
-        description: "Описание события 2",
-        date: "2021-05-20",
-        organizer: "Организатор 2",
-        user: [{
-            name: "Saret",
-            email: 'saret@ghgjg.com'
-        }],
-    },
-    {
-        id: 3,
-        title: "Событие 3",
-        description: "Описание события 3",
-        date: "2024-08-20",
-        organizer: "Организатор 1"
-    },
-    {
-        id: 4,
-        title: "Событие 4",
-        description: "Описание события 4",
-        date: "2024-08-25",
-        organizer: "Организатор 2"
-    },
-    {
-        id: 5,
-        title: "Событие 5",
-        description: "Описание события 5",
-        date: "2025-05-20",
-        organizer: "Организатор 1"
-    },
-    {
-        id: 6,
-        title: "Событие 6",
-        description: "Описание события 6",
-        date: "2025-01-25",
-        organizer: "Организатор 2"
-    },
-    {
-        id: 7,
-        title: "Событие 7",
-        description: "Описание события 7",
-        date: "2022-05-20",
-        organizer: "Организатор 1"
-    },
-    {
-        id: 8,
-        title: "Событие 8",
-        description: "Описание события 8",
-        date: "2023-02-25",
-        organizer: "Организатор 2"
-    },
-    {
-        id: 9,
-        title: "Событие 9",
-        description: "Описание события 9",
-        date: "2024-05-24",
-        organizer: "Организатор 1"
-    },
-    {
-        id: 10,
-        title: "Событие 10",
-        description: "Описание события 10",
-        date: "2024-06-25",
-        organizer: "Организатор 2",
-        users: [],
-    },
-    {
-        id: 11,
-        title: "Событие 11",
-        description: "Описание события 11",
-        date: "2023-03-20",
-        organizer: "Организатор 1",
-        users: [],
-    },
-    {
-        id: 12,
-        title: "Событие 12",
-        description: "Описание события 12",
-        date: "2024-05-25",
-        organizer: "Организатор 2",
-        users: [],
-    },
+let events = []
 
-    // Добавьте больше событий по аналогии
-];
 
 const itemsPerPage = 4; // Количество событий на странице
 
@@ -116,36 +12,45 @@ const pagination = document.getElementById('pagination');
 let currentPage = 1;
 
 // Функция для отображения событий на текущей странице
+
 function displayEvents(page) {
+
     eventList.innerHTML = '';
 
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const displayedEvents = events.slice(startIndex, endIndex);
 
-    displayedEvents.forEach(events => {
+    displayedEvents.forEach(ev => {
 
         const eventElement = document.createElement('div');
         eventElement.classList.add('event');
-        eventElement.dataset.id = events.id
+        eventElement.dataset.id = ev.id
         eventElement.innerHTML = `
-            <h2>Title: ${events.title}</h2>
-            <p><strong>Data:</strong> ${events.date}</p>
-            <p><strong>Organiser:</strong> ${events.organizer}</p>
-            <p>Description: ${events.description}</p>
+            <h2>Title: ${ev.title}</h2>
+            <p><strong>Data:</strong> ${ev.date}</p>
+            <p><strong>Organiser:</strong> ${ev.organizer}</p>
+            <p>Description: ${ev.description}</p>
             <div class="control">
-                <button id="${events.id}" type="button" class="registr">Redistr</button>     
-                <button id="${events.id}" type="button" class="btn-view">View</button>
+                <button id="${ev.id}" type="button" class="registr">Redistr</button>     
+                <button id="${ev.id}" type="button" class="btn-view">View</button>
             </div>
             
         `;
         eventList.appendChild(eventElement)
 
-
     });
 
 }
-
+const initApp = () => {
+    fetch("./event.json")
+        .then((response) => response.json())
+        .then((data) => {
+            events = data.devents;
+            displayEvents(1);
+        });
+};
+initApp();
 
 
 // Функция для отображения пагинации
@@ -210,11 +115,7 @@ const toggleModal = () => {
     mod.classList.toggle('body-mod')
 };
 
-
-
-// modalOpen.addEventListener('click', toggleModal);
 modalClose.addEventListener('click', toggleModal);
-
 
 
 
--- scripts.js
+++ scripts.js
@@ -1,125 +1,6 @@
 
-const events = [
-    {
-        id: 1,
-        title: "Событие 1",
-        description: "Описание события 1",
-        date: "2023-05-10",
-        organizer: "Организатор 1",
-        user: [
-            {
-            name: "Alex",
-            email: 'alex@ghgjg.com'
-        },{
-            name: "Stif",
-            email: 'stif@ghgjg.com'
-        },{
-            name: "Dron",
-            email: 'dron@ghgjg.com'
-        },
-
-        ],
-    },
-    {
-        id: 2,
-        title: "Событие 2",
-        description: "Описание события 1",
-        date: "2021-05-20",
-        organizer: "Организатор 1",
-        user: [{
-            name: "Saret",
-            email: 'saret@ghgjg.com'
-        }],
-    },
-    {
-        id: 3,
-        title: "Событие 3",
-        description: "Описание события 3",
-        date: "2024-08-20",
-        organizer: "Организатор 1",
-        user: [],
-    },
-    {
-        id: 4,
-        title: "Событие 4",
-        description: "Описание события 4",
-        date: "2024-08-25",
-        organizer: "Организатор 2",
-        user: [],
-    },
-    {
-        id: 5,
-        title: "Событие 5",
-        description: "Описание события 5",
-        date: "2025-05-20",
-        organizer: "Организатор 1",
-        user: [],
-    },
-    {
-        id: 6,
-        title: "Событие 6",
-        description: "Описание события 6",
-        date: "2025-01-25",
-        organizer: "Организатор 2",
-        user: [],
-    },
-    {
-        id: 7,
-        title: "Событие 7",
-        description: "Описание события 7",
-        date: "2022-05-20",
-        organizer: "Организатор 1",
-        user: [],
-    },
-    {
-        id: 8,
-        title: "Событие 8",
-        description: "Описание события 8",
-        date: "2023-02-25",
-        organizer: "Организатор 2",
-        user: [],
-    },
-    {
-        id: 9,
-        title: "Событие 9",
-        description: "Описание события 9",
-        date: "2024-05-24",
-        organizer: "Организатор 1",
-        user: [],
-    },
-    {
-        id: 10,
-        title: "Событие 10",
-        description: "Описание события 10",
-        date: "2024-06-25",
-        organizer: "Организатор 2",
-        user: [],
-    },
-    {
-        id: 11,
-        title: "Событие 11",
-        description: "Описание события 11",
-        date: "2023-03-20",
-        organizer: "Организатор 1",
-        user: [],
-    },
-    {
-        id: 12,
-        title: "Событие 12",
-        description: "Описание события 12",
-        date: "2024-05-25",
-        organizer: "Организатор 2",
-        user: [],
-    },
-
-];
-
-// ======================== VIEW ============================
-
-
 
 const idBook = Number(localStorage.getItem("button"));
-console.log(idBook)
 let bookItem = document.querySelector(".event-list");
 
 const addDataToHTML = () => {
@@ -152,4 +33,15 @@ const addDataToHTML = () => {
             }
         });
 };
-addDataToHTML();
+
+const initApp = () => {
+    fetch("./event.json")
+        .then((response) => response.json())
+        .then((data) => {
+            events = data.devents;
+            addDataToHTML();
+        });
+};
+initApp();
+
+
